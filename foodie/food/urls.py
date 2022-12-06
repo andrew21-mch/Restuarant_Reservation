78ff9db0a0572cfbb841d0384fd81cded013ae71
anne <annemachinda@gmail.com>
2022-12-06 19:55:07 +0100

    new view

--- foodie/food/urls.py
+++ foodie/food/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('food', views.food, name='food'),
+
     # path('about/', views.about, name='about'),
     # path('contact/', views.contact, name='contact'),
 ]
--- foodie/food/views.py
+++ foodie/food/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def food(request):
+    return render(request, 'food.html')
