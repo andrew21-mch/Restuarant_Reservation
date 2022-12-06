153274383064ac882253548b68fe82f74cd1deb0
njein <njeinicodona@gmail.com>
2022-12-06 14:06:39 +0100

    small changes

--- foodie/food/views.py
+++ foodie/food/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    new = User.objects.all()
+    return HttpResponse(f'<h1> welcome yoo</h1> <h2> three amigos</h2>')
+
